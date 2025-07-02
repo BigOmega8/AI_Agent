b65d848322a623033298034cd11f12ce31fc64a8
Shuddhasatya Biswas <shuddhasatya.biswas1997@gmail.com>
2025-07-02 10:46:54 +0530

    Added Jupyter Notebook

--- src/documents/models.py
+++ src/documents/models.py
@@ -9,8 +9,18 @@ User = settings.AUTH_USER_MODEL # -> 'auth.User' or custom user model
 class Document(models.Model):
     owner = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(default = 'Title')
-    content = models.TextField(default = 'Content')
+    content = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
-    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, default=timezone.now)
+    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        if self.active and self.active_at is None:
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
